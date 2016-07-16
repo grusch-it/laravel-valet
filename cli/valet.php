40a75619454a12c8d46c7b715ba93af7cffb566d
Michael Gruschwitz <info@grusch-it.de>
2016-07-16 15:26:13 +0200

    Bump version

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$version = '1.1.13.1';
+$version = '1.1.13.2';
 
 $app = new Application('Laravel Valet', $version);
 
